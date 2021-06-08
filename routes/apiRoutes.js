e78944380cfe0227f085377231dded5d942bb3b2
Darren Watson <v_tec@live.com>
2021-06-08 17:13:01 -0500

    had trouble with the workout api but finally got it

---
+++ routes/apiRoutes.js
@@ -0,0 +1,82 @@
+const Workout = require('../models/workout');
+
+module.exports = app => {
+
+// Create a new workout
+app.post('/api/workouts', (req, res) => {
+    Workout.create({})
+    .then(workoutDB => {
+        console.log(workoutDB);
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        console.log(err)
+        res.json(err);
+    });
+});
+
+// Update existing workout
+app.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercise: req.body } },
+        { new: true, runValidators: true }
+    )
+    .then(workoutDB => {
+        res.json(workoutDB)
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+// get all workouts, add exercise duration
+app.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercise.duration',
+                },
+            },
+        },
+    ])
+    .then(workoutDB => {
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+app.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercise.duration',
+                },
+            },
+        },
+    ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then(workoutDB => {
+        console.log(workoutDB);
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+// app.delete('/api/workouts', ({ body }, res) => {
+//     Workout.findByIdAndDelete(body.id)
+//     .then(() => {
+//         res.json(true);
+//     })
+//     .catch(err => {
+//         res.json(err);
+//     });
+// });
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,12 +11,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-app.use(require("./routes/api.js"));
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
